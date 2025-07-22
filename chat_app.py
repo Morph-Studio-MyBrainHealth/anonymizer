76a72d0d58ef8b8ccc810ea467cc6692ba3cf598
Kotz <greg.kotzbauer@gmail.com>
2025-07-21 21:10:21 -0400

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -510,5 +510,5 @@ def stats_endpoint():
         return jsonify({'error': str(e)}), 500
 
     if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port, debug=False)
